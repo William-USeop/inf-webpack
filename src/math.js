39f5ba951b1615fa9cce2d34d036d46f9923350f
Son William <>
2022-02-19 21:57:39 +0900

    import export 간단 예제

---
+++ src/math.js
@@ -0,0 +1,23 @@
+// function sum(a,b) {
+//     return a+b;
+// }
+
+// var math = math || {};
+
+// 즉시실행을 하고 var라는 타입은 호이스팅이 가능하다.
+// (function () {
+    
+//     function sum(a,b) {
+//         return a+b;
+//     }
+ 
+
+//     math.sum = sum;
+
+    
+// })()
+
+
+export function sum(a,b) {
+    return a+b;
+}
